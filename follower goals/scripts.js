21b8bd23a0add5e6f85a0e959856c18e5ecf3a61
gretanevera <kika0125@gmail.com>
2020-08-19 17:48:35 +0300

    animation done

--- follower goals/scripts.js
+++ follower goals/scripts.js
@@ -1,23 +1,43 @@
 console.log("hello world!")
 
-let finalGoal= 500;
+let finalGoal= 300;
 let onePercent= finalGoal / 100;
-let currentProgress= 300;
+let currentFollows = 50
+let currentProgress= currentFollows/ onePercent ;
+
+document.getElementById("progress-bar-complete").style.width = currentProgress+"%";
+
+console.log(document.getElementById("goal-number").value)
+
+
 // document.getElementsByClassName('currentProg').addEventListener("onchange", currentChanged());
+
 
 
 function currentChanged() {
 console.log('values changed')
 
-     let vals= parseInt(document.getElementsByClassName('currentProg').value);
+     let vals= parseInt(document.getElementById('currentProg').value);
     
-    let barProgress = vals / onePercent;
+    let barProgress = (vals / onePercent);
+
+// if(barLength < predetermined lenght){
+// change it to predetertimed lenght
+
+// }else{
+//     keep it that way
+// } id one with media, delete thi100
+
 
     console.log('vals is ' + vals);
     console.log('percentage is ' + barProgress);
 
-    // document.getElementsByClassName('progress-bar-compete').style.width=barProgress + "%";
+    document.getElementById("progress-bar-complete").style.width = barProgress+"%";
+
+
+
 }
+
 function goalReached() {
     //poof its done
 }
