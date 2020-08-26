472ecf198482eb075db1c127de5107c16f28028b
gretanevera <kika0125@gmail.com>
2020-08-26 16:01:55 +0300

    startig on goal bar

--- follower goals/scripts.js
+++ follower goals/scripts.js
@@ -3,9 +3,47 @@ let currentGoal = parseInt(document.getElementById('goal').value);
 let currentProgress = currentFollows / (currentGoal / 100);
 let starterFollowerCount = parseInt(document.getElementById('starter-followers').value);
 
+let goalDetails = {
+    1: {
+        "name": "Play a horror game",
+        "startingPoint": 25,
+        "goal":50
+    },
+    2: {
+         "name": "The game you want to play",
+        "startingPoint": 50,
+        "goal":100
+    },
+    3: {
+       "name": "???",
+        "startingPoint": 100,
+        "goal":200
+    },
+    4: {
+         "name": "Gaming with viewers",
+        "startingPoint": 200,
+        "goal":500
+    },
+    5: {
+         "name": "???",
+        "startingPoint": 500,
+        "goal":1000
+    }
+}
+
+
 document.getElementById("achieved-number").textContent = currentFollows;
 document.getElementById("goal-number").textContent = currentGoal;
 document.getElementById("progress-bar-complete").style.width = currentProgress + "%";
+
+function levelChecker(){
+//check now many followers we have
+//adjust the goal level
+//render the needed thing
+
+}
+
+
 
 
 function currentChanged() {
@@ -16,24 +54,11 @@ function currentChanged() {
     let vals = parseInt(document.getElementById('currentProg').value);
     let barProgress = (vals - starterFollowerCount) / onePercent;
 
-    document.getElementById("achieved-number").textContent = vals;
-    console.log("current followers is " + vals);
-    console.log("current bar progress is " + barProgress);
-    console.log("current starter follower couny is " + starterFollowerCount);
-    console.log("current one percent is " + onePercent);
-
-
-
     if (barProgress < 100) {
         document.getElementById("progress-bar-complete").style.width = barProgress + "%";
     } else if (barProgress > 100) {
         goalReached()
     }
-
-    // } else if (barProgress <= 0) {
-    //     //call a function that delevels the bar
-    //     console.log("you have been demoted");
-    // }
 
 }
 
@@ -44,12 +69,9 @@ function onGoalChange() {
 }
 
 function goalReached() {;
-    //to do :
-    //make that when it reaches one 100 percent the goal becomes minimum value. 
-    //count percentage from that minimum
+    // to do :
+    // make that when it reaches one 100 percent the goal becomes minimum value.
+    // count percentage from that minimum
     // poof its done
+    //call levelChecker
 }
-
-// add function that get the info
-// add a function changes the width of the bar and numbers
-// animate that shiz
---
+++ for chrams/script.js
@@ -0,0 +1,49 @@
+// have a follower
+// someone follows
+// poof or smokey fadeout?
+// new follower
+// ???
+// profit
+
+let initialTxr = 'follower_ocks';
+document.getElementById("latest").innerHTML = initialTxr;
+
+
+function wait(ms) {
+    var d = new Date();
+    var d2 = null;
+    do {
+        d2 = new Date();
+    } while (d2 - d < ms);
+}
+
+function chageInitiate() {
+    let changedPrimary = JSON.stringify(document.getElementById("change-input").value);
+    let changed = changedPrimary.replace(/"/g, "");
+     // poof
+    document.getElementById("poof").style.opacity = "1";
+    document.getElementById("latest").style.opacity = "0";
+
+
+
+
+    setTimeout(function () {
+        document.getElementById("latest").innerHTML = changed;
+        document.getElementById("latest").style.opacity = "1";
+
+    }, 1000);
+
+    // wait
+    setTimeout(function () {
+        document.getElementById("poof").style.opacity = "0"
+    }, 1500);
+
+
+}
+
+
+// ideas for other transition:
+// like a neon sign
+// someone follows
+// sign flickers goes off
+// flickers on with a new name
